326ad80c01ffc3f6a30da343b67379fd166b57a1
VishaalR <vishaalr333@gmail.com>
2026-05-30 22:45:04 +0530

    Update preprocessing.py

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -5,7 +5,6 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
-# Download NLTK resources
 nltk.download('punkt')
 nltk.download('punkt_tab')
 nltk.download('stopwords')
